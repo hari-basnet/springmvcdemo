c92beb3ea42d68673c6a9cf2809cd34a63c71c1b
hari-basnet <harikrishnabsnt@gmail.com>
2021-01-17 23:01:22 +0200

    initial commit

---
+++ src/test/java/com/habasnet/springdemo/SpringmvcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habasnet.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

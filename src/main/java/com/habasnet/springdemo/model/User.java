fb4d65f551b370cbb1f842feb413d306ed81c4b8
hari-basnet <harikrishnabsnt@gmail.com>
2021-01-21 00:16:42 +0200

    adding hibernate support

--- src/main/java/com/habasnet/springdemo/model/User.java
+++ src/main/java/com/habasnet/springdemo/model/User.java
@@ -1,7 +1,14 @@
 package com.habasnet.springdemo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private int id;
 	private String firstName;
 	private String lastName;

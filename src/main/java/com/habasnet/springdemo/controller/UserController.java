b424c252789fa459855f64baac1f96d462d2d946
hari-basnet <harikrishnabsnt@gmail.com>
2021-01-20 23:22:18 +0200

    Created login and signup form and added routes to controller

--- src/main/java/com/habasnet/springdemo/controller/UserController.java
+++ src/main/java/com/habasnet/springdemo/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.habasnet.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.habasnet.springdemo.model.User;
+
+import jdk.jfr.Frequency;
 
 @Controller
 public class UserController {
@@ -17,5 +23,24 @@ public class UserController {
 	public String getSignupForm() {
 
 		return "signupForm";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String loginUser(@ModelAttribute User user, Model model) {
+		
+		if(user.getUserName().equals("ram") && user.getPassword().equals("helsinki")) {
+			
+			model.addAttribute("username", user.getUserName());
+			return "home";
+		}
+		
+		model.addAttribute("message", "User not found!!");
+		return "loginForm";
+	}
+	
+	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	public String userSignup(@ModelAttribute User user) {
+		
+		return "redirect:/login";
 	}
 }
